5d5f9928c5ed09d9171148dc6ef18f5945d82d7b
hh-h <hh-h@users.noreply.github.com>
2019-09-17 04:12:43 +0300

    fixed bug recent uvloop doesn't work

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,5 +28,4 @@ def pytest_generate_tests(metafunc):
     if "host" in metafunc.fixturenames:
         metafunc.parametrize("host", ["127.0.0.1"])
     if "port" in metafunc.fixturenames:
-        print(1)
         metafunc.parametrize("port", [int(os.environ["KOSHH/AIOSNMP_161_UDP"])])
