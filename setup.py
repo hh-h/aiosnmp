db8f6e35da95033ecc1fcf7435005ae4a7b41fc1
Konstantin Valetov <forjob@thetrue.name>
2021-01-23 10:40:47 +0300

    freeze deps in tox.ini

--- setup.py
+++ setup.py
@@ -1,34 +1,36 @@
-import aiosnmp
 from pathlib import Path
+
 from setuptools import setup
 
-readme = Path(__file__).with_name('README.md')
+import aiosnmp
+
+readme = Path(__file__).with_name("README.md")
 
 setup(
-    name='aiosnmp',
+    name="aiosnmp",
     version=aiosnmp.__version__,
-    packages=['aiosnmp'],
-    url='https://github.com/hh-h/aiosnmp',
-    license='MIT',
-    author='Valetov Konstantin',
-    author_email='forjob@thetrue.name',
-    description='asyncio SNMP client',
-    long_description=readme.read_text('utf-8'),
-    long_description_content_type='text/markdown',
+    packages=["aiosnmp"],
+    url="https://github.com/hh-h/aiosnmp",
+    license="MIT",
+    author="Valetov Konstantin",
+    author_email="forjob@thetrue.name",
+    description="asyncio SNMP client",
+    long_description=readme.read_text("utf-8"),
+    long_description_content_type="text/markdown",
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Development Status :: 4 - Beta',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Framework :: AsyncIO',
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Framework :: AsyncIO",
     ],
-    python_requires='>=3.6'
+    python_requires=">=3.6",
 )
