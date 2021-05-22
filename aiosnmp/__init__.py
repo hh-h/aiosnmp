ef5fbe9e1e8da936b4e99ae34e3f056a8cf93d90
Konstantin Valetov <forjob@thetrue.name>
2021-05-22 20:50:46 +0300

    0.5.0

--- aiosnmp/__init__.py
+++ aiosnmp/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ("Snmp", "SnmpV2TrapMessage", "SnmpV2TrapServer", "exceptions", "SnmpVarbind")
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __author__ = "Valetov Konstantin"
 
 from .message import SnmpV2TrapMessage, SnmpVarbind
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Valetov Konstantin'
 author = 'Valetov Konstantin'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.1'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
