131a68c9f8b645f6726015e990b90c6287522d0e
hh-h <hh-h@users.noreply.github.com>
2022-01-07 19:16:13 +0300

    Update __init__.py

--- aiosnmp/__init__.py
+++ aiosnmp/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ("Snmp", "SnmpV2TrapMessage", "SnmpV2TrapServer", "exceptions", "SnmpVarbind")
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 __author__ = "Valetov Konstantin"
 
 from .message import SnmpV2TrapMessage, SnmpVarbind
