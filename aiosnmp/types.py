8ed18ff596a1f9ed221afc0d802d72c30fba171a
Soji Thomas <Soji.Thomas@cox.com>
2022-08-18 19:59:15 -0400

    Add Gauge32 value range constraint.

--- aiosnmp/types.py
+++ aiosnmp/types.py
@@ -1,6 +1,12 @@
 class Gauge32:
     def __init__(self, val: int):
+        if val < 0 or val > 4294967295:
+            raise Gauge32ValueError('rfc1902: Gauge32 is a positive int between 0 and 4294967295')
         self._value = val
 
     def get_value(self):
         return self._value
+
+
+class Gauge32ValueError(ValueError):
+    """A Value Error related to the SNMP Gauge32 type."""
